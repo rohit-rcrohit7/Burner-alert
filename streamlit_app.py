94212430c5f0a66b6c26c0dcfc992e4b555137ee
Rohit Chakraborty <rcrohit7@gmail.com>
2024-07-10 12:31:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,8 +138,7 @@ st.markdown("""
 
 # Streamlit app title
 st.markdown("<h1 class='main-title'>Burner Alert</h1>", unsafe_allow_html=True)
-st.subheader('Find the burner alert status in Swansea', divider='rainbow')
-st.write(':red[Enter your postcode] to determine if it is safe to use your wood stove')
+st.subheader(':red[Enter your postcode] to determine if it is safe to use your wood stove')
 
 # Input for postcode
 postcode = st.text_input('Enter your postcode:')
