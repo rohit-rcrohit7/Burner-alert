95c5e432df8f64383352aa0c4d133d428864c60b
Rohit Chakraborty <rcrohit7@gmail.com>
2024-07-11 16:39:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,7 @@ if page == "Burner Alert Status":
    
 
     # Input for postcode
-    postcode = st.text_input('Enter your postcode:')
+    postcode = st.text_input()
 
     if postcode:
         # Standardize the postcode input
